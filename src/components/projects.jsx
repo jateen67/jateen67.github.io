bbb07f5b51dd88949468b81c2a2d5ce8e473ebc8
exit <kalsijatin67@gmail.com>
2023-07-29 00:10:07 -0400

    update

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -14,6 +14,93 @@ export default function Projects() {
             >
               <img
                 className="actual-project-image"
+                src="microservice.png"
+                alt="microservice pic"
+              />
+            </div>
+            <div className="project-description project-description-blue">
+              <h2
+                className="project-title"
+                data-aos="fade-left"
+                data-aos-delay="600"
+              >
+                Distributed System
+              </h2>
+              <div
+                className="project-text"
+                data-aos="fade-left"
+                data-aos-delay="700"
+              >
+                Scalable app built with microservice architecture
+              </div>
+              <div className="project-buttons">
+                <a
+                  className="cta cta2"
+                  href="https://github.com/jateen67/gomicroservices"
+                  target="_blank"
+                >
+                  <span>CODE</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="project-2" data-aos="slide-right">
+          <div className="project-contents">
+            <div className="project-description">
+              <h2
+                className="project-title"
+                data-aos="fade-right"
+                data-aos-delay="600"
+              >
+                AI Stock Recommender
+              </h2>
+              <div
+                className="project-text"
+                data-aos="fade-right"
+                data-aos-delay="700"
+              >
+                Machine learning model that recommends stocks
+              </div>
+              <div className="project-buttons">
+                <a
+                  className="cta"
+                  href="https://aistockrecommender.web.app/"
+                  target="_blank"
+                >
+                  <span>SITE</span>
+                </a>
+                <a
+                  className="cta cta2"
+                  href="https://github.com/jateen67/stock"
+                  target="_blank"
+                >
+                  <span>CODE</span>
+                </a>
+              </div>
+            </div>
+            <div
+              className="project-pic"
+              data-aos="fade-right"
+              data-aos-delay="500"
+            >
+              <img
+                className="actual-project-image-2"
+                src="stock.png"
+                alt="stock pic"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="project" data-aos="slide-left">
+          <div className="project-contents">
+            <div
+              className="project-pic"
+              data-aos="fade-left"
+              data-aos-delay="500"
+            >
+              <img
+                className="actual-project-image"
                 src="social media.png"
                 alt="social media site"
               />
@@ -31,7 +118,7 @@ export default function Projects() {
                 data-aos="fade-left"
                 data-aos-delay="700"
               >
-                Social media platform inspired by Reddit
+                Join communities and share posts with others
               </div>
               <div className="project-buttons">
                 <a
@@ -60,39 +147,41 @@ export default function Projects() {
                 data-aos="fade-right"
                 data-aos-delay="600"
               >
-                Music Album Database
+                Travelling Salesman Problem Visualizer
               </h2>
               <div
                 className="project-text"
                 data-aos="fade-right"
                 data-aos-delay="700"
               >
-                Store and view all your favourite music albums
+                Visualize and learn about this infamous problem
               </div>
               <div className="project-buttons">
                 <a
                   className="cta"
-                  href="https://albumdatabasefrontend.azurewebsites.net/"
+                  href="https://tsp-visualizer.web.app/"
                   target="_blank"
                 >
                   <span>SITE</span>
                 </a>
                 <a
                   className="cta cta2"
-                  href="https://github.com/jateen67/albumdbcsharp"
+                  href="https://github.com/jateen67/tsp"
                   target="_blank"
                 >
                   <span>CODE</span>
                 </a>
               </div>
             </div>
-            <div className="project-pic">
+            <div
+              className="project-pic"
+              data-aos="fade-right"
+              data-aos-delay="500"
+            >
               <img
                 className="actual-project-image-2"
-                src="album.png"
-                alt="music album pic"
-                data-aos="fade-right"
-                data-aos-delay="500"
+                src="tspgif.gif"
+                alt="tsp visualizer pic"
               />
             </div>
           </div>
@@ -137,93 +226,6 @@ export default function Projects() {
             </div>
           </div>
         </div>
-        <div className="project-2" data-aos="slide-right">
-          <div className="project-contents">
-            <div className="project-description">
-              <h2
-                className="project-title"
-                data-aos="fade-right"
-                data-aos-delay="600"
-              >
-                Video Game Deals Finder
-              </h2>
-              <div
-                className="project-text"
-                data-aos="fade-right"
-                data-aos-delay="700"
-              >
-                Find the cheapest online deals in PC gaming
-              </div>
-              <div className="project-buttons">
-                <a
-                  className="cta"
-                  href="https://gamedealsapi.web.app/"
-                  target="_blank"
-                >
-                  <span>SITE</span>
-                </a>
-                <a
-                  className="cta cta2"
-                  href="https://github.com/jateen67/gameapi"
-                  target="_blank"
-                >
-                  <span>CODE</span>
-                </a>
-              </div>
-            </div>
-            <div
-              className="project-pic"
-              data-aos="fade-right"
-              data-aos-delay="500"
-            >
-              <img
-                className="actual-project-image-2"
-                src="game.gif"
-                alt="video game deals pic"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="project" data-aos="slide-left">
-          <div className="project-contents">
-            <div
-              className="project-pic"
-              data-aos="fade-left"
-              data-aos-delay="500"
-            >
-              <img
-                className="actual-project-image"
-                src="microservice.png"
-                alt="microservice pic"
-              />
-            </div>
-            <div className="project-description project-description-blue">
-              <h2
-                className="project-title"
-                data-aos="fade-left"
-                data-aos-delay="600"
-              >
-                Distributed System
-              </h2>
-              <div
-                className="project-text"
-                data-aos="fade-left"
-                data-aos-delay="700"
-              >
-                Scalable app built with microservice architecture
-              </div>
-              <div className="project-buttons">
-                <a
-                  className="cta cta2"
-                  href="https://github.com/jateen67/gomicroservices"
-                  target="_blank"
-                >
-                  <span>CODE</span>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
         <div className="last-project" data-aos="slide-right">
           <div className="project-contents">
             <div className="project-description">
@@ -232,41 +234,32 @@ export default function Projects() {
                 data-aos="fade-right"
                 data-aos-delay="600"
               >
-                AI Stock Recommender
+                Music Album Database
               </h2>
               <div
                 className="project-text"
                 data-aos="fade-right"
                 data-aos-delay="700"
               >
-                Machine learning model that recommends stocks
+                Store and view all your favourite music albums
               </div>
               <div className="project-buttons">
                 <a
-                  className="cta"
-                  href="https://aistockrecommender.web.app/"
-                  target="_blank"
-                >
-                  <span>SITE</span>
-                </a>
-                <a
                   className="cta cta2"
-                  href="https://github.com/jateen67/stock"
+                  href="https://github.com/jateen67/albumdbcsharp"
                   target="_blank"
                 >
                   <span>CODE</span>
                 </a>
               </div>
             </div>
-            <div
-              className="project-pic"
-              data-aos="fade-right"
-              data-aos-delay="500"
-            >
+            <div className="project-pic">
               <img
                 className="actual-project-image-2"
-                src="stock.png"
-                alt="stock pic"
+                src="album.png"
+                alt="music album pic"
+                data-aos="fade-right"
+                data-aos-delay="500"
               />
             </div>
           </div>
